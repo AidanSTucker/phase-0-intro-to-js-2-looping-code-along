5ab241ebedd747d74c5c32e0213d481f9813df0f
elpatron <aidanseantucker@gmail.com>
2023-04-14 11:08:26 -0400

    Finshed both tests, birthday cars, & incrementing down from 10

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+
+
+function writeCards(name, eventName) {
+    let messagesArray = [];
+    
+
+    for(let i = 0; i < name.length; i++) {
+        let message =  `Thank you, ${name[i]}, for the wonderful ${eventName} gift!`;
+        messagesArray.push(message);
+    }
+
+    return messagesArray
+}
+
+
+
+function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  
